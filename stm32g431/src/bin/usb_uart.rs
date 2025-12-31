722ca12dc614d19cd5f221174a3950e7625f63e6
Ole Bauck <ole.bauck@remarkable.no>
2025-12-31 11:54:13 +0100

    Usb uart run function generic.

--- stm32g431/src/bin/usb_uart.rs
+++ stm32g431/src/bin/usb_uart.rs
@@ -10,7 +10,7 @@ use embassy_stm32::usart::BufferedUart;
 use embassy_stm32::usb::Driver;
 use embassy_stm32::{bind_interrupts, peripherals, usart, usb, Config, Peri};
 use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
-// use embassy_usb::driver::Driver as UsbDriver;
+use embassy_usb::driver::Driver as UsbDriver;
 use {defmt_rtt as _, panic_probe as _};
 
 use embedded_io_async::{BufRead, Write};
@@ -21,80 +21,19 @@ bind_interrupts!(struct Irqs {
     USART1 => usart::BufferedInterruptHandler<peripherals::USART1>;
 });
 
-// pub struct Runner<'d, D: UsbDriver<'d>, U> {
-//     cdc_acm: CdcAcmClass<'d, D>,
-//     usart: U,
-// }
-
-// impl<'d, D, U> Runner<'d, D, U>
-// where
-//     U: BufRead + Write,
-//     D: UsbDriver<'d>,
-// {
-//     pub async fn run(mut self) -> ! {
-//         let mut usb_buf = [0; 64];
-
-//         let (mut usb_tx, mut usb_rx, usb_control) = self.cdc_acm.split_with_control();
-
-//         loop {
-//             join(usb_rx.wait_connection(), usb_tx.wait_connection()).await;
-//             info!("Connected");
-//             loop {
-//                 match select3(
-//                     usb_control.control_changed(),
-//                     usb_rx.read_packet(&mut usb_buf),
-//                     self.usart.fill_buf(),
-//                 )
-//                 .await
-//                 {
-//                     Either3::First(_) => {
-//                         info!("Line coding: {:?}", Debug2Format(&usb_rx.line_coding()));
-//                     }
-//                     Either3::Second(res) => match res {
-//                         Err(err) => {
-//                             error!("Disconnected {:?}", err);
-//                             break;
-//                         }
-//                         Ok(n) => {
-//                             if let Err(err) = self.usart.write(&usb_buf[0..n]).await {
-//                                 error!("Unable to write to usart: {:?}", Debug2Format(&err));
-//                             }
-//                         }
-//                     },
-//                     Either3::Third(res) => {
-//                         let usart_buf = match res {
-//                             Err(err) => {
-//                                 error!("usart buf error: {:?}", Debug2Format(&err));
-//                                 continue;
-//                             }
-//                             Ok(buf) => buf,
-//                         };
-//                         if let Err(err) = usb_tx.write_packet(usart_buf).await {
-//                             error!("Disconnected {:?}", err);
-//                             break;
-//                         }
-//                         let n = usart_buf.len();
-//                         self.usart.consume(n);
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-pub trait UsartBaud {
+pub trait UartBaud {
     fn set_baud(&mut self, baudrate: u32);
 }
 
-impl<'d> UsartBaud for BufferedUart<'d> {
+impl<'d> UartBaud for BufferedUart<'d> {
     fn set_baud(&mut self, baudrate: u32) {
         let _ = self.set_baudrate(baudrate);
     }
 }
 
-pub async fn run(
-    mut usart: impl BufRead + Write + UsartBaud,
-    cdc_acm: CdcAcmClass<'static, Driver<'static, peripherals::USB>>,
+pub async fn run<'d, D: UsbDriver<'d>>(
+    mut usart: impl BufRead + Write + UartBaud,
+    cdc_acm: CdcAcmClass<'d, D>,
 ) -> ! {
     let mut usb_buf = [0; 64];
 
