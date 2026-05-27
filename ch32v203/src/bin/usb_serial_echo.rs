6b51bb93f7047a813af61e7183f9059a84caa692
Ole Bauck <ole.bauck@remarkable.no>
2026-05-27 20:45:26 +0200

    Examples: Add usb serial echo examples and bandwidth test results.

---
+++ ch32v203/src/bin/usb_serial_echo.rs
@@ -0,0 +1,129 @@
+#![no_std]
+#![no_main]
+
+use ch32_hal as hal;
+use embassy_executor::Spawner;
+use embassy_futures::join::join;
+use embassy_futures::select::{select, Either};
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use hal::bind_interrupts;
+use hal::peripherals::USBD;
+use hal::usbd::Driver;
+use static_cell::StaticCell;
+
+#[macro_export]
+macro_rules! my_println {
+    ($($arg:tt)*) => {
+        ()
+    };
+}
+
+// Println fucks up because it is blocking
+// When no debugger is connected, the fw just hangs
+// TODO: Switch to ringbuffer + defmt (see defmt_rtt crate for inspiration or use this: https://crates.io/crates/defmt-bbq)
+use my_println as println;
+
+bind_interrupts!(struct Irqs {
+    USB_LP_CAN1_RX0 => hal::usbd::InterruptHandler<hal::peripherals::USBD>;
+});
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    // let _ = println!("\n\n\n{}", _info);
+    println!("Panic!");
+
+    loop {}
+}
+
+#[embassy_executor::task]
+async fn usb_uart_task(class: CdcAcmClass<'static, Driver<'static, USBD>>) -> ! {
+    let mut buf = [0; 64];
+
+    let (mut usb_tx, mut usb_rx, usb_control) = class.split_with_control();
+
+    loop {
+        join(usb_rx.wait_connection(), usb_tx.wait_connection()).await;
+        println!("Connected");
+        loop {
+            match select(usb_control.control_changed(), usb_rx.read_packet(&mut buf)).await {
+                Either::First(_) => {
+                    let baud = usb_rx.line_coding().data_rate();
+                    println!("Baud change: {:?}", baud);
+                }
+                Either::Second(res) => match res {
+                    Err(err) => {
+                        println!("Usb rx error: {:?}. Assume disconnection", err);
+                        break;
+                    }
+                    Ok(n) => {
+                        // Echo data
+                        if let Err(err) = usb_tx.write_packet(&buf[..n]).await {
+                            println!("Usb tx error: {:?}. Assume disconnection", err);
+                            break;
+                        }
+                    }
+                },
+            }
+        }
+    }
+}
+
+// If you are trying this and your USB device doesn't connect, the most
+// common issues are the RCC config and vbus_detection
+//
+// See https://embassy.dev/book/#_the_usb_examples_are_not_working_on_my_board_is_there_anything_else_i_need_to_configure
+// for more information.
+//
+// println will not work without probe.
+#[embassy_executor::main(entry = "qingke_rt::entry")]
+async fn main(spawner: Spawner) {
+    let p = hal::init(hal::Config {
+        rcc: hal::rcc::Config::SYSCLK_FREQ_144MHZ_HSI,
+        ..Default::default()
+    });
+
+    let driver = Driver::new(p.USBD, Irqs, p.PA12, p.PA11);
+
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xC0DE, 0xCAFE);
+    config.manufacturer = Some("Bauck");
+    config.product = Some("OB-Link (COM port)");
+    config.serial_number = Some("12345678");
+
+    // Windows compatibility requires these; CDC-ACM
+    config.device_class = 0x02;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x00;
+    config.composite_with_iads = false;
+
+    let mut builder = {
+        static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+        static BOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+        static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
+
+        let builder = embassy_usb::Builder::new(
+            driver,
+            config,
+            CONFIG_DESCRIPTOR.init([0; 256]),
+            BOS_DESCRIPTOR.init([0; 256]),
+            &mut [], // no msos descriptors
+            CONTROL_BUF.init([0; 64]),
+        );
+        builder
+    };
+
+    // Create classes on the builder.
+    let class = {
+        static STATE: StaticCell<State> = StaticCell::new();
+        let state = STATE.init(State::new());
+        CdcAcmClass::new(&mut builder, state, 64)
+    };
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    spawner.spawn(usb_uart_task(class)).unwrap();
+
+    // Run the USB device.
+    usb.run().await;
+}
---
+++ stm32g431/src/bin/usb_serial_echo.rs
@@ -0,0 +1,127 @@
+#![no_std]
+#![no_main]
+
+use defmt::{info, warn};
+use embassy_executor::Spawner;
+use embassy_futures::join::join;
+use embassy_futures::select::{Either, select};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::usb::Driver;
+use embassy_stm32::{Config, bind_interrupts, peripherals, usb};
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use {defmt_rtt as _, panic_probe as _};
+
+use static_cell::StaticCell;
+
+bind_interrupts!(struct Irqs {
+    USB_LP => usb::InterruptHandler<peripherals::USB>;
+});
+
+#[embassy_executor::task(pool_size = 2)]
+async fn usb_uart_task(class: CdcAcmClass<'static, Driver<'static, peripherals::USB>>) -> ! {
+    let mut buf = [0; 64];
+
+    let (mut usb_tx, mut usb_rx, usb_control) = class.split_with_control();
+
+    loop {
+        join(usb_rx.wait_connection(), usb_tx.wait_connection()).await;
+        info!("Connected");
+        loop {
+            match select(usb_control.control_changed(), usb_rx.read_packet(&mut buf)).await {
+                Either::First(_) => {
+                    let baud = usb_rx.line_coding().data_rate();
+                    info!("Baud change: {:?}", baud);
+                }
+                Either::Second(res) => match res {
+                    Err(err) => {
+                        warn!("Usb rx error: {:?}. Assume disconnection", err);
+                        break;
+                    }
+                    Ok(n) => {
+                        // Echo data
+                        if let Err(err) = usb_tx.write_packet(&buf[..n]).await {
+                            warn!("Usb tx error: {:?}. Assume disconnection", err);
+                            break;
+                        }
+                    }
+                },
+            }
+        }
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        // Sets up the Clock Recovery System (CRS) to use the USB SOF to trim the HSI48 oscillator.
+        config.rcc.hsi48 = Some(Hsi48Config {
+            sync_from_usb: true,
+        });
+        config.rcc.hse = Some(Hse {
+            freq: Hertz(8_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll = Some(Pll {
+            source: PllSource::HSE,
+            prediv: PllPreDiv::DIV2,
+            mul: PllMul::MUL72,
+            divp: None,
+            divq: Some(PllQDiv::DIV6), // 48mhz
+            divr: Some(PllRDiv::DIV2), // Main system clock at 144 MHz
+        });
+        config.rcc.sys = Sysclk::PLL1_R;
+        config.rcc.boost = true; // BOOST!
+        config.rcc.mux.clk48sel = mux::Clk48sel::HSI48;
+        //config.rcc.mux.clk48sel = mux::Clk48sel::PLL1_Q; // uncomment to use PLL1_Q instead.
+    }
+    let p = embassy_stm32::init(config);
+
+    info!("STM32G431 USB uart example");
+
+    let driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
+
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xC0DE, 0xCAFE);
+    config.manufacturer = Some("Bauck");
+    config.product = Some("OB-Link (COM port)");
+    config.serial_number = Some("12345678");
+
+    // Windows compatibility requires these; CDC-ACM
+    config.device_class = 0x02;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x00;
+    config.composite_with_iads = false;
+
+    let mut builder = {
+        static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+        static BOS_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
+        static CONTROL_BUF: StaticCell<[u8; 64]> = StaticCell::new();
+
+        let builder = embassy_usb::Builder::new(
+            driver,
+            config,
+            CONFIG_DESCRIPTOR.init([0; 256]),
+            BOS_DESCRIPTOR.init([0; 256]),
+            &mut [], // no msos descriptors
+            CONTROL_BUF.init([0; 64]),
+        );
+        builder
+    };
+
+    // Create classes on the builder.
+    let class = {
+        static STATE: StaticCell<State> = StaticCell::new();
+        let state = STATE.init(State::new());
+        CdcAcmClass::new(&mut builder, state, 64)
+    };
+
+    spawner.spawn(usb_uart_task(class)).unwrap();
+
+    // Build the builder.
+    let mut usb = builder.build();
+
+    // Run the USB device.
+    usb.run().await;
+}
